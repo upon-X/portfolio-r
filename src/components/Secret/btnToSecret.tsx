9de1b6f600205e166ea89c4678718c9239c1e62e
upon-X <valemiche003@gmail.com>
2024-05-09 22:22:00 -0300

    styliong secret page

--- src/components/Secret/btnToSecret.tsx
+++ src/components/Secret/btnToSecret.tsx
@@ -6,6 +6,7 @@ export const Secret = () => {
       <Link
         to="/secret-game"
         className="sm:hidden md:hidden absolute bottom-20 left-20 opacity-0 hover:opacity-100 px-3 py-2 rounded-full bg-purple text-white"
+        onClick={() => sessionStorage.setItem("token-to-secret-game", "true")}
       >
         SECRET
       </Link>
--- src/pages/SecretGame.tsx
+++ src/pages/SecretGame.tsx
@@ -1,3 +1,26 @@
+import { FaBan } from "react-icons/fa6";
+import { useTranslation } from "../context/TranslationContext";
+
 export default function SecretGame() {
-  return <></>;
+  const { language } = useTranslation();
+  const token = sessionStorage.getItem("token-to-secret-game");
+  if (token === "true") {
+    return <>true</>;
+  } else {
+    return (
+      <p className="flex justify-center items-center sm:flex-col sm:text-center h-[100vh] text-xl font-semibold uppercase">
+        <span className="flex items-center gap-2">
+          <span className="text-red">
+            <FaBan />
+          </span>
+          {language === "es"
+            ? "No tienes acceso a esta pagina"
+            : "You do not have access to this page"}
+          <span className="text-red">
+            <FaBan />
+          </span>
+        </span>
+      </p>
+    );
+  }
 }
