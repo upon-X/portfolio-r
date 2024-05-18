288cbc48d59adad18e0810ca28f48bc1b53e05b6
upon-X <valemiche003@gmail.com>
2024-05-18 01:06:33 -0300

    changing png to webp image

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from "../context/TranslationContext";
 import { Description } from "../components/AboutMe/Description";
 import { MySkils } from "../components/AboutMe/MySkills";
-import myPhoto from "../assets/myphotonobg.png";
+import myPhoto from "../assets/myphotonobg.webp";
 import { useEffect } from "react";
 import "aos/dist/aos.css";
 import AOS from "aos";
@@ -20,21 +20,18 @@ export default function AboutMe() {
     ${theme === "light" ? "" : "bg-darkTheme text-white"}
     `}
     >
-      {/* <h3
-        data-aos="fade-down"
-        className="flex flex-col gap-3 items-center uppercase text-4xl font-semibold"
-      >
-        {content?.aboutMe[language].title}
-        <span className="w-[100px] border-b-[6px] rounded-full border-purple text-transparent"></span>
-      </h3> */}
       <h4 data-aos="fade-down" className="text-center text-2xl">
         {content?.aboutMe[language].subtitle}
       </h4>
       <div
         data-aos="fade-in"
-        className="w-[230px] overflow-hidden rounded-full border-4 border-purple "
+        className=" overflow-hidden rounded-full border-4 border-purple "
       >
-        <img src={myPhoto} alt="Valentino Micheloni Photo" />
+        <img
+          className="w-[230px] h-[230px]"
+          src={myPhoto}
+          alt="Valentino Micheloni Photo"
+        />
       </div>
       <div className="grid grid-cols-2 gap-16">
         <div>
