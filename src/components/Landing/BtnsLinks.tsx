7b8edd4d93ea9a87da78a32745068fb18d468424
upon-X <valemiche003@gmail.com>
2024-05-18 00:55:51 -0300

    improving all lighthouse but 57 performance Landing

--- src/components/Landing/BtnsLinks.tsx
+++ src/components/Landing/BtnsLinks.tsx
@@ -11,18 +11,22 @@ export const BtnsLinks = () => {
     AOS.init({ duration: 1000 });
   });
   return (
-    <div data-aos="fade-up" className="flex justify-center gap-10 text-purple">
+    <div
+      data-aos="fade-up"
+      className="flex justify-center gap-10 md:gap-12 text-purple"
+    >
       <Link
+        aria-label="Read more about me, my skills and my experience on IT"
         to={"/about-me"}
         className="flex flex-col items-center justify-center p-3 bg-transparent rounded-md border-x-4 border-purple
           hover:-translate-y-2 hover:text-white hover:bg-purple group
           transition-all
           "
       >
-        <FaRegAddressCard className="w-[3rem] h-[3rem]" />
+        <FaRegAddressCard className="w-[3rem] h-[3rem] md:w-[2.3rem] md:h-[2.3rem] sm:w-[2rem] sm:h-[2rem]" />
         <p
           className="hidden absolute text-xl whitespace-nowrap text-purple font-bold
-        group-hover:block group-hover:-mb-[6.5rem] transition-all uppercase
+        group-hover:block group-hover:-mb-[6.5rem] transition-all uppercase 
         "
         >
           {language === "es" ? "Sobre mi" : "About me"}
@@ -30,16 +34,17 @@ export const BtnsLinks = () => {
       </Link>
 
       <Link
+        aria-label="Read more about my projects and visit their deploy"
         to={"/projects"}
         className="flex items-center justify-center p-3 bg-transparent rounded-md border-x-4 border-purple
           hover:-translate-y-2 hover:text-white hover:bg-purple group
           transition-all
           "
       >
-        <FaCode className="w-[3rem] h-[3rem]" />
+        <FaCode className="w-[3rem] h-[3rem] md:w-[2.3rem] md:h-[2.3rem] sm:w-[2rem] sm:h-[2rem]" />
         <p
           className="hidden absolute text-xl whitespace-nowrap text-purple font-bold
-        group-hover:block group-hover:-mb-[6.5rem] transition-all uppercase
+        group-hover:block group-hover:-mb-[6.5rem] transition-all uppercase 
         "
         >
           {language === "es" ? "Proyectos" : "Projects"}
@@ -47,16 +52,17 @@ export const BtnsLinks = () => {
       </Link>
 
       <Link
+        aria-label="Go on and contact me by filling an email-form"
         to={"/contact"}
         className="flex items-center justify-center p-3 bg-transparent rounded-md border-x-4 border-purple
           hover:-translate-y-2 hover:text-white hover:bg-purple group
           transition-all
           "
       >
-        <FaRegMessage className="w-[3rem] h-[3rem]" />
+        <FaRegMessage className="w-[3rem] h-[3rem] md:w-[2.3rem] md:h-[2.3rem] sm:w-[2rem] sm:h-[2rem]" />
         <p
           className="hidden absolute text-xl whitespace-nowrap text-purple font-bold
-        group-hover:block group-hover:-mb-[6.5rem] transition-all uppercase
+        group-hover:block group-hover:-mb-[6.5rem] transition-all uppercase 
         "
         >
           {language === "es" ? "Contacto" : "Contact"}
--- src/components/Landing/Presentation.tsx
+++ src/components/Landing/Presentation.tsx
@@ -19,11 +19,11 @@ export default function Presentation() {
         className="flex flex-col items-center text-center"
       >
         <h1
-          className={`uppercase text-8xl font-medium font-juliusFF 2xl:-mb-6 whitespace-nowrap 
-        xl:text-7xl xl:-mb-4
-        lg:text-6xl lg:-mb-2
-        md:text-7xl md:whitespace-normal md:leading-[1.1]
-        sm:text-6xl sm:whitespace-normal sm:leading-[1.1] 
+          className={`uppercase text-6xl font-medium font-juliusFF 2xl:-mb-5 whitespace-nowrap 
+        2xl:text-7xl xl:-mb-4
+        lg:-mb-2
+        md:whitespace-normal md:leading-[1.1]
+        sm:text-5xl sm:whitespace-normal sm:leading-[1.1] 
         drop-shadow-[0_5px_5px_rgba(0,0,0,0.25)]
         ${theme === "light" ? "text-darkTheme" : "text-white"}
         `}
@@ -32,11 +32,12 @@ export default function Presentation() {
         </h1>
         <div className=" bg-purple w-full ">
           <h2
-            className={`flex items-center justify-center gap-1 text-white font-semibold text-5xl uppercase font-jostFF whitespace-nowrap 
-          xl:text-4xl
-          lg:text-3xl
-          md:text-3xl
-          sm:text-2xl drop-shadow-[0_5px_5px_rgba(0,0,0,0.25)]
+            className={`flex items-center justify-center gap-1 text-white font-semibold text-4xl uppercase font-jostFF whitespace-nowrap 
+          xl:text-3xl
+          lg:text-2xl
+          md:text-2xl
+          sm:text-xl sm:px-1
+          drop-shadow-[0_5px_5px_rgba(0,0,0,0.25)]
           `}
           >
             <span className="font-josefinFF sm:hidden">{"<"}</span>
