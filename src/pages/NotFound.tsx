97816a284cae78a64d947afa712a13eac4d9ffe4
upon-X <valemiche003@gmail.com>
2024-05-18 01:37:30 -0300

    improving notfound

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -14,25 +14,25 @@ export default function NotFound() {
       }`}
     >
       <div className="flex justify-center items-center flex-col">
-        <div>
-          <span className="text-[10rem] hover:text-purple transition-all">
-            4
-          </span>
-          <span className="text-[10rem] hover:text-purple transition-all">
-            0
-          </span>
-          <span className="text-[10rem] hover:text-purple transition-all">
-            4
-          </span>
-        </div>
+        <span className="text-[9.5rem] hover:text-purple transition-all">
+          404
+        </span>
         <span className="text-[2rem]">" {pathname} "</span>
         <p className="text-[2rem]">{content?.notFound[language].title}</p>
         <p className="text-[1.2rem]">{content?.notFound[language].tryUrls}</p>
-        <div className="flex flex-col text-purple text-[1.2rem]">
-          <Link to={"/"}>"/"</Link>
-          <Link to={"/about-me"}>"/about-me"</Link>
-          <Link to={"/projects"}>"/projects"</Link>
-          <Link to={"/contact"}>"/contact"</Link>
+        <div className="flex flex-col text-purple text-[1.2rem] mt-8">
+          <Link aria-label="A link to the landing page" to={"/"}>
+            {'> "/"'}
+          </Link>
+          <Link aria-label="A link to about-me page" to={"/about-me"}>
+            {'> "/about-me"'}
+          </Link>
+          <Link aria-label="A link to projects page" to={"/projects"}>
+            {'> "/projects"'}
+          </Link>
+          <Link aria-label="A link to contact page" to={"/contact"}>
+            {'> "/contact"'}
+          </Link>
         </div>
         <Secret />
       </div>
