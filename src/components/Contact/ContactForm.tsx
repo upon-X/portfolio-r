057b56f2dbe254ed3f991a406c05cc19ce4b83e2
upon-X <valemiche003@gmail.com>
2024-05-20 20:08:49 -0300

    responsive contact

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -47,8 +47,12 @@ export const ContactForm = () => {
     <form
       id="form"
       onSubmit={handleSubmit}
-      className={`flex flex-col items-end gap-4 h-[450px] text-xl rounded-lg shadow-lg p-8
+      className={`flex flex-col items-end gap-4 2xl:h-[450px] text-xl rounded-lg shadow-lg p-8
       2xl:w-[800px] 2xl:grid 2xl:grid-cols-2 2xl:grid-row-3
+      xl:w-[700px] xl:grid xl:grid-cols-2 xl:grid-row-3
+      lg:w-[500px]
+      md:w-[400px]
+      sm:w-[90vw] sm:px-4
       ${theme === "light" ? "" : "bg-[#2c2c2c]"}
       `}
     >
@@ -80,10 +84,9 @@ export const ContactForm = () => {
           onChange={(e) => setEmail(e.target.value)}
           maxLength={100}
           required
-          pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
         />
       </label>
-      <label className="flex flex-col gap-1 w-full 2xl:col-span-2">
+      <label className="flex flex-col gap-2 w-full 2xl:col-span-2 xl:col-span-2">
         {content?.contact[language].form.message}
         <textarea
           className={`text-lg rounded-md min-h-[200px] max-h-[200px] outline-purple focus:outline-purple p-2 resize-none
@@ -98,10 +101,10 @@ export const ContactForm = () => {
           required
         />
       </label>
-      <div className=" flex justify-center col-start-2 ">
+      <div className=" flex justify-center 2xl:col-start-2 xl:col-start-2 w-full sm:mt-2">
         <button
           type="submit"
-          className="px-14 py-2 uppercase font-figtreeFF font-semibold transition-all 2xl:w-full outline-purple
+          className="px-14 py-2 uppercase font-figtreeFF font-semibold transition-all 2xl:w-full w-full outline-purple
           active:scale-95 
           border-2 border-purple rounded-full hover:bg-purple hover:text-white
         "
