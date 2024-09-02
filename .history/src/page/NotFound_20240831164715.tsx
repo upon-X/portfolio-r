a1ebf7f3c88622bc671a19b82c8aa692a24e5a33
upon-X <valemiche003@gmail.com>
2024-09-01 22:12:35 -0300

    Added Malt link

---
+++ .history/src/page/NotFound_20240831164715.tsx
@@ -0,0 +1,48 @@
+import { Link, useLocation } from "react-router-dom";
+import { useTranslation } from "../context/TranslationContext";
+import { Secret } from "../components/Secret/btnToSecret";
+import { useTheme } from "../context/ThemeContext";
+
+export default function NotFound() {
+  const { language, content } = useTranslation();
+  const { theme } = useTheme();
+  const { pathname } = useLocation();
+  return (
+    <section
+      className={`h-[92vh] select-none ${
+        theme === "light" ? "" : "bg-darkTheme text-white"
+      }`}
+    >
+      <div className="flex justify-center items-center flex-col py-4">
+        <span className="text-[9.5rem] hover:text-purple transition-all">
+          404
+        </span>
+        <span className="text-[2rem]">" {pathname} "</span>
+        <p className="text-[2rem]">{content?.notFound[language].title}</p>
+        <div
+          className={`flex flex-col ${
+            theme === "light" ? "text-darkTheme hover:text-white" : "text-white"
+          } text-[1.2rem] mt-10 font-semibold `}
+        >
+          <Link
+            aria-label="A link to the landing page"
+            className=" border-2 border-purple py-1 px-4 rounded-full hover:bg-purple transition-all"
+            to={"/"}
+          >
+            {content?.notFound[language].tryUrls}
+          </Link>
+        </div>
+        <Secret />
+      </div>
+    </section>
+  );
+}
+
+const name = "vale nico";
+const name2 = "nico";
+
+if (name.includes(name2)) {
+  console.log("esta incluido");
+} else {
+  console.log(" no esta incluido");
+}
